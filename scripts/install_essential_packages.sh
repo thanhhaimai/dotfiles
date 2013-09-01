6d5e37415a99c4a063850abba4ff9961b52b8b65
Thanh Hai Mai <thanhhaipmai@gmail.com>
2013-09-01 15:50:28 -0700

    + add scripts to install essential packages

---
+++ scripts/install_essential_packages.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo add-apt-repository ppa:salience-team/salience-devel-ppa
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-get update
+sudo apt-get install salience-theme
+sudo apt-get install delorean-dark
+
+sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+libcairo2-dev libx11-dev libxpm-dev libxt-dev ruby-dev mercurial \
+trash-cli ack-grep tig build-essential cmake python-dev
+
+sudo apt-get install compizconfig-settings-manager fusion-icon \
+compiz-plugins-extra libdecoration0-dev
+
