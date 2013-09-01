84a361f6da6c8e7252bbb3a26d6e7ec6f255f3a4
Thanh Hai Mai <thanhhaipmai@gmail.com>
2013-09-01 15:55:27 -0700

    ~ set x permission

--- scripts/install_essential_packages.sh
+++ scripts/install_essential_packages.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get upgrade
-
 sudo add-apt-repository ppa:salience-team/salience-devel-ppa
 sudo add-apt-repository ppa:noobslab/themes
 sudo apt-get update
+sudo apt-get upgrade
+
 sudo apt-get install salience-theme
 sudo apt-get install delorean-dark
 
