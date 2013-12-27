19712665b94a79c3e19b4b544ed53b19e6ca7618
Thanh Hai Mai <thanhhaipmai@gmail.com>
2013-12-26 19:35:58 -0800

    ~ move install_xmonad and setup_cronjobs to tuna's script

--- setup.sh
+++ setup.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 baseDir=$(dirname $(readlink -f $0))
+hostname=$(hostname)
 
 set -e
 
 echo "============================= BEGIN ============================"
 $baseDir/scripts/install_essential_packages.sh
 $baseDir/scripts/setup_rc.sh
-$baseDir/scripts/install_xmonad.sh
 $baseDir/scripts/install_python.sh
 $baseDir/scripts/install_fasd.sh
 $baseDir/scripts/install_vundle.sh
 $baseDir/scripts/install_ycm.sh
 $baseDir/scripts/install_go.sh
 $baseDir/scripts/install_vim_golang.sh
-$baseDir/scripts/setup_cronjobs.sh
 $baseDir/scripts/setup_autostart.sh
+# $baseDir/scripts/install_xmonad.sh
 # $baseDir/scripts/install_vim.sh
 # $baseDir/scripts/install_compiz.sh
 # $baseDir/scripts/install_dark_themes.sh
+# $baseDir/scripts/setup_cronjobs.sh
+
+if [ -f "$baseDir/machines/$hostname/scripts/setup.sh" ]; then
+    $baseDir/machines/$hostname/scripts/setup.sh
+fi
 echo "============================= DONE ============================"
 
 echo "Reloading settings..."
