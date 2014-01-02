d745aa7e3c53ec9fefdcd7b921f3a2a214ebb7d2
Thanh Hai Mai <thanhhaipmai@gmail.com>
2014-01-01 18:35:01 -0800

    ~ update dark themes list

--- scripts/install_dark_themes.sh
+++ scripts/install_dark_themes.sh
@@ -1,9 +1,10 @@
-echo "Installing LinuxMint dark themes..."
+echo "Installing dark themes..."
 
-sudo add-apt-repository ppa:salience-team/salience-devel-ppa
-sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository ppa:noobslab/themes --yes
 
 sudo apt-get update --yes
 
-sudo apt-get install salience-theme
-sudo apt-get install delorean-dark
+sudo apt-get install delorean-dark --yes
+sudo apt-get install zoncolor-themes --yes
+sudo apt-get install zoncolor-icons --yes
+sudo apt-get install zen-suite --yes
