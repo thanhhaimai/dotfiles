88e7e6ccd8664a09fe83a1502287c56be8ad738f
Hai Mai <thanhhaipmai@gmail.com>
2024-03-14 12:38:44 -0700

    remove unnecessary `rg` dependency

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -12,7 +12,7 @@ touch ~/.ssh/known_hosts
 sudo chmod 644 ~/.ssh/known_hosts
 
 # Register all the keys
-for file in $(find ~/.ssh | rg ".*id_ed25519_[a-z0-9_]+$")
+for file in $(find ~/.ssh | grep -E ".*id_ed25519_[a-z0-9_]+$")
 do
   ssh-add "$file"
 done
