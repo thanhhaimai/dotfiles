5128e9a95747255b7c55f8dff530cd3c8f1440c2
Hai Mai <thanhhaipmai@gmail.com>
2024-03-14 11:05:58 -0700

    add setup bazel

--- setup_bazel.sh
+++ setup_bazel.sh
@@ -5,20 +5,8 @@
 # Execute in the source dir of the script, regardless where invoked from.
 cd $(dirname "$0")
 
-./setup_system.sh
-./setup_ssh.sh
-./setup_fonts.sh
-
-./setup_rust.sh
-./setup_pip.sh
-
-./setup_git.sh
-./setup_vim.sh
-
-./setup_zsh.sh
-./setup_kitty.sh
-#./setup_i3.sh
-
-./setup_bazel.sh
+# Set up config for bazel
+rm -rf ~/.bazelrc
+ln -s $(readlink -f .bazelrc) ~
 
 } # protect against editing while executing
