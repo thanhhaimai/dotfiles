5f2aeb25d4c2cf2df6fa8ab94c49d880005f667c
Hai Mai <thanhhaipmai@gmail.com>
2025-03-11 23:19:19 -0700

    Fix nvim setup

--- work/.config/nvim/lua/plugins/config.lua
+++ work/.config/nvim/lua/plugins/config.lua
@@ -5,22 +5,25 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-	{
-		"folke/flash.nvim",
+  {
+    "folke/flash.nvim",
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, false },
       { "S", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     },
-	},
-	{
-		"saghen/blink.cmp",
-		opts = {
-			keymap = {
-				preset = "super-tab",
-			},
-		},
-		version = "0.13.0",
-		fuzzy = { implementation = "rust" },
-	},
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "super-tab",
+      },
+    },
+    version = "0.13.0",
+    fuzzy = { implementation = "rust" },
+  },
+  {
+    "lambdalisue/vim-suda",
+  },
 }
